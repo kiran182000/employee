cb57fec25268948cdf484a84f859fe43cd231645
kiran <kiran405411@gmail.com>
2024-01-28 15:03:52 +0530

    employe wage

--- src/Empwage.java
+++ src/Empwage.java
@@ -5,7 +5,7 @@ public class Empwage {
         int emphrs=0;
         int empwage=0;
         double empcheck=Math.floor(Math.random()*10)%2;
-        if(empcheck==full_time)
+        if(empcheck==is_full_time)
             emphrs=8;
         else
             emphrs=0;
