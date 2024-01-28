e57a1ef1f89339cbf0cb57b56369159365923386
kiran <kiran405411@gmail.com>
2024-01-28 15:32:13 +0530

    empwage using switch

--- src/Empwage.java
+++ src/Empwage.java
@@ -1,6 +1,26 @@
 public class Empwage {
+    public static final int is_full_time=2;
+    public static final int is_part_time=1;
+    public static final int rph=20;
     public static void main(String[]args){
-        System.out.println("welcome");
+        int emphrs=0;
+        int empwage=0;
+        int empcheck=(int) Math.floor(Math.random()*10)%3;
+        switch (empcheck){
+            case is_part_time:
+                emphrs=4;
+                break;
+            case is_full_time:
+                emphrs=8;
+                break;
+            default:
+                emphrs=0;
+        }
+        empwage=emphrs*rph;
+        System.out.println("empwage: "+ empwage);
+
+
+
 
     }
 }
