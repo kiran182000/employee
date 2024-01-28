920beb66df8a7e9cb51fdcd6f5f8ec2c250f409c
kiran <kiran405411@gmail.com>
2024-01-28 14:52:50 +0530

    employe wage

--- src/Empwage.java
+++ src/Empwage.java
@@ -1,6 +1,19 @@
 public class Empwage {
     public static void main(String[]args){
-        System.out.println("welcome");
+        int full_time=1;
+        int rph=20;
+        int emphrs=0;
+        int empwage=0;
+        double empcheck=Math.floor(Math.random()*10)%2;
+        if(empcheck==full_time)
+            emphrs=8;
+        else
+            emphrs=0;
+        empwage=emphrs*rph;
+        System.out.println("empwage:" +empwage);
+
+
+
 
     }
 }
