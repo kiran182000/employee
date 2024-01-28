d626525797f9c79a06d0a4e6847a22581945e1d9
kiran <kiran405411@gmail.com>
2024-01-28 15:21:13 +0530

    part time wage

--- src/Empwage.java
+++ src/Empwage.java
@@ -1,6 +1,22 @@
 public class Empwage {
     public static void main(String[]args){
-        System.out.println("welcome");
+        int is_full_time=2;
+        int is_part_time=1;
+        int rph=20;
+        int emphrs=0;
+        int empwage=0;
+        double empchcek=Math.floor(Math.random()*10)%3;
+        if(empchcek==is_part_time)
+            emphrs=4;
+        else if (empchcek==is_full_time)
+        emphrs=8;
+        else
+        emphrs=0;
+        empwage=emphrs*rph;
+        System.out.println("empwage:" +empwage);
+        {
+
+        }
 
     }
 }
