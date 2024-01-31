60bf37d7300cb21a8da58a3309209cde3658627f
kiran <kiran405411@gmail.com>
2024-01-31 12:05:47 +0530

    total empwage for companies

--- src/Empwage.java
+++ src/Empwage.java
@@ -1,11 +1,19 @@
 public  class Empwage {
     public static final int is_full_time=2;
     public static final int is_part_time=1;
-    public static final int rph=20;
-    public static final int no_of_working_days=20;
-    public static final int max_hrs_in_month=100;
+    private final String company;
+    private final int rph;
+    private final int no_of_working_days;
+    private final int max_hrs_in_month;
+    private int totalempwage;
+    public Empwage(String company,int rph,int no_of_working_days,int max_hrs_in_month){
+        this.company=company;
+        this.rph=rph;
+        this.no_of_working_days=no_of_working_days;
+        this.max_hrs_in_month=max_hrs_in_month;
+    }
 
-    public static int computeempwage(){
+    public void computeempwage(){
         int emphrs=0,totalemphrs=0,totalworkingdays=0;
         while(totalemphrs<=max_hrs_in_month&&totalworkingdays<no_of_working_days){
             totalworkingdays++;
@@ -23,12 +31,20 @@ public  class Empwage {
             totalemphrs+=emphrs;
             System.out.println("Day: "+totalworkingdays+ "emp hr:" +emphrs);
         }
-        int totalwage=totalemphrs*rph;
-        System.out.println("Total emp wage:" +totalwage);
-        return totalwage;
+        totalempwage=totalemphrs*rph;
+    }
+    public String toString(){
+        return "Total emp wage for company: " +company+ " is: " + totalempwage;
     }
     public static void main(String[] args){
-        computeempwage();
+        Empwage abc =  new Empwage("ABC",10,2,10);
+        Empwage xyz = new Empwage("XYZ",8,3,7);
+        abc.computeempwage();
+        System.out.println(abc);
+        xyz.computeempwage();
+        System.out.println(xyz);
     }
 }
+
+
 
