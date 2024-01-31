515a85cb95dc7ea785440abee70483032680a9d1
kiran <kiran405411@gmail.com>
2024-01-31 11:54:49 +0530

    Multiple company

--- src/Empwage.java
+++ src/Empwage.java
@@ -1,11 +1,7 @@
 public  class Empwage {
     public static final int is_full_time=2;
     public static final int is_part_time=1;
-    public static final int rph=20;
-    public static final int no_of_working_days=20;
-    public static final int max_hrs_in_month=100;
-
-    public static int computeempwage(){
+    public static int computeempwage(String company,int rph,int no_of_working_days,int max_hrs_in_month){
         int emphrs=0,totalemphrs=0,totalworkingdays=0;
         while(totalemphrs<=max_hrs_in_month&&totalworkingdays<no_of_working_days){
             totalworkingdays++;
@@ -23,12 +19,13 @@ public  class Empwage {
             totalemphrs+=emphrs;
             System.out.println("Day: "+totalworkingdays+ "emp hr:" +emphrs);
         }
-        int totalwage=totalemphrs*rph;
-        System.out.println("Total emp wage:" +totalwage);
-        return totalwage;
+        int totalempwage=totalemphrs*rph;
+        System.out.println("Total emp wage for company: " + company+ " is: " + totalempwage);
+        return totalempwage;
     }
     public static void main(String[] args){
-        computeempwage();
+        computeempwage("ABC",20,2,10);
+        computeempwage("XYZ",8,5,15);
     }
 }
 
